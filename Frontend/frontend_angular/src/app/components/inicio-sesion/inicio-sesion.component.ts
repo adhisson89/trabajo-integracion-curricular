0a21a787db3c1c3edf908b76622eaeb66111050a
Ivonne-Ramirez <ivonne.ramirez@epn.edu.ec>
2024-11-04 19:37:55 -0500

    front_InicioSesion

--- Frontend/frontend_angular/src/app/components/inicio-sesion/inicio-sesion.component.ts
+++ Frontend/frontend_angular/src/app/components/inicio-sesion/inicio-sesion.component.ts
@@ -1,18 +1,39 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 
 @Component({
   selector: 'app-inicio-sesion',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule], // Agrega CommonModule aquí
   templateUrl: './inicio-sesion.component.html',
-  styleUrl: './inicio-sesion.component.css'
+  styleUrls: ['./inicio-sesion.component.css']
 })
 export class InicioSesionComponent {
+  loginForm: FormGroup;
+
   constructor(
     private router: Router,
-    
-  ) { }
+    private fb: FormBuilder
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      console.log('Usuario:', username);
+      console.log('Contraseña:', password);
+      
+      // Redirecciona a una ruta específica después del inicio de sesión exitoso
+      this.redirectTo('/ruta-destino');
+    }
+  }
+
   redirectTo(route: string) {
     this.router.navigate([route]);
   }
