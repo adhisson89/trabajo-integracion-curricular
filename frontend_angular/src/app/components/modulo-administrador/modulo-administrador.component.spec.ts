ac6735d5f7b6ddff5a4e40879749608d9e455d9e
Ivonne-Ramirez <ivonne.ramirez@epn.edu.ec>
2024-12-23 17:54:33 -0500

    pruebas unitarias registros

--- frontend_angular/src/app/components/modulo-administrador/modulo-administrador.component.spec.ts
+++ frontend_angular/src/app/components/modulo-administrador/modulo-administrador.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ModuloAdministradorComponent } from './modulo-administrador.component';
+import { By } from '@angular/platform-browser';
+
 
 describe('ModuloAdministradorComponent', () => {
   let component: ModuloAdministradorComponent;
@@ -19,4 +21,36 @@ describe('ModuloAdministradorComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+//Prueba unitaria para la redirección (redirectTo)
+it('should call redirectTo with correct route', () => {
+  // Espiamos la función router.navigate
+  spyOn(component['router'], 'navigate');
+  
+  // Llamamos a la función con una ruta de ejemplo
+  const route = 'moduloRegistro';
+  component.redirectTo(route);
+
+  // Comprobamos si la función navigate fue llamada con la ruta esperada
+  expect(component['router'].navigate).toHaveBeenCalledWith([route]);
+ console.log('FrontEnd Policia Nacional del Ecuador Prueba de redirección ejecutada. La función navigate fue llamada con la ruta: ', route);
+});
+
+
+//Prueba unitaria para verificar si las clases CSS están aplicadas correctamente:
+
+it('should apply the correct CSS classes to module divs', () => {
+  const moduleElements = fixture.debugElement.queryAll(By.css('.module'));
+  
+  // Verificamos que el número de elementos sea correcto
+  expect(moduleElements.length).toBe(2);
+  moduleElements.forEach((module) => {
+    const classList = module.nativeElement.classList;
+    expect(classList).toContain('module');
+   console.log('FrontEnd Policia Nacional del Ecuador Clase CSS "module" aplicada correctamente:', classList);
+  });
+});
+
+
 });
