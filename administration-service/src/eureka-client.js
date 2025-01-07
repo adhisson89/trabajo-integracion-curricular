cf40a207c6ca2127c5b37539cb2ad9bad5f8233d
Adhisson Cedeño <adhisson89@hotmail.com>
2025-01-07 15:43:28 -0500

    feat: Mejorar configuración del cliente Eureka y manejo de procesos

--- administration-service/src/eureka-client.js
+++ administration-service/src/eureka-client.js
@@ -1,22 +1,22 @@
-const Eureka = require('eureka-js-client').Eureka;
+const { Eureka } = require('eureka-js-client');
 const os = require('os');
 const crypto = require('crypto');
 require("dotenv").config();
 
 // Genera un ID aleatorio para la instancia
 const instanceId = `administration-service:${crypto.randomBytes(8).toString('hex')}`;
-const port = process.env.PORT;
+
 
 // Configura el cliente Eureka
 const client = new Eureka({
-    // Configuración de la instancia del microservicio
     instance: {
         app: 'administration-service',
-        instanceId: instanceId,
         hostName: os.hostname(),
-        ipAddr: 'localhost',
+        ipAddr: '127.0.0.1',
+        instanceId,
+        statusPageUrl: `http://localhost:${process.env.PORT}/test/health`,
         port: {
-            '$': port,
+            '$': process.env.PORT,
             '@enabled': 'true',
         },
         vipAddress: 'administration-service',
@@ -25,7 +25,6 @@ const client = new Eureka({
             name: 'MyOwn',
         },
     },
-    // Configuración de conexión con el servidor Eureka
     eureka: {
         host: '172.25.0.3',
         port: 8761,
@@ -33,7 +32,31 @@ const client = new Eureka({
     }
 });
 
-client.start(error => {
-    console.log('Eureka client started with error:', error);
+client.logger.level('debug')
+
+// Inicia el cliente Eureka
+client.start( error => {
+    console.log(error || "User service registered")
 });
+
+function exitHandler(options, exitCode) {
+    if (options.cleanup) {
+    }
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    if (options.exit) {
+        client.stop();
+    }
+}
+
+client.on('deregistered', () => {
+    console.log('after deregistered');
+    process.exit();
+})
+
+client.on('started', () => {
+  console.log("eureka host  " + '172.25.0.3');
+})
+
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+process.on('SIGTERM', exitHandler.bind(null, {exit:true}));
 
--- administration-service/src/index.js
+++ administration-service/src/index.js
@@ -2,14 +2,15 @@ const app = require("./app.js");
 const connectDB = require("./database.js");
 
 require("dotenv").config();
-console.log("Variable de ambiente", process.env.MONGO_URI);
 connectDB();
 
 const port = Math.floor(Math.random() * (3999 - 3000 + 1)) + 3000;
 process.env.PORT = port;
+const PORT = process.env.PORT || 3000;
 
 require('./eureka-client');
 
-const PORT = process.env.PORT || 3000;
+connectDB();
 
-app.listen(PORT, () => console.log("La API se levanto en ", PORT));
+
+app.listen(PORT, () => console.log(`La API se levantó en el puerto ${PORT}`));
