317fa5a2b4ea71169144a7338ef27578b0e70280
Adhisson Cedeño <adhisson89@hotmail.com>
2025-01-21 15:57:31 -0500

    Removed old backend

--- administration-service/src/libs/multer.lib.js
+++
@@ -1,7 +0,0 @@
-const multer = require('multer');
-
-// Configuración de multer
-const storage = multer.memoryStorage(); // Almacena la imagen en memoria antes de guardarla en MongoDB
-const upload = multer({ storage });
-
-module.exports = upload;
